dc775f32131db2074ca40626950fb1ee8bb7412c
Michael Schneider <schneider.mic@gmx.de>
2018-06-02 14:22:10 +0200

    added initial version of Tower

---
+++ TheClottingDefender/Assets/Entities/Tower.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+
+	private GameObject Target;
+	public int Cooldown;
+	private int CurrentCooldown;
+	public int Health;
+	public int Range;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CurrentCooldown < 0)
+		{
+			CurrentCooldown--;
+		}
+		else
+		{
+			Shoot();
+		}
+	}
+
+	private void Shoot()
+	{
+		// #TODO: implement logic to shoot on target enemy
+		CurrentCooldown = Cooldown;
+	}
+}
