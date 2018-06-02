2b5041832c63802b9d858c0caeeb5242f2b79f51
Michael Schneider <schneider.mic@gmx.de>
2018-06-02 13:59:36 +0200

    added simple mouseOver functionality for Tile

--- TheClottingDefender/Assets/Entities/Tile.cs
+++ TheClottingDefender/Assets/Entities/Tile.cs
@@ -2,15 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tile : MonoBehaviour {
+public class Tile : MonoBehaviour
+{
+
+	private SpriteRenderer SpriteRenderer;
 
 	// Use this for initialization
 	void Start () {
-		
+		Debug.Log("Starting");
+		SpriteRenderer = GetComponent<SpriteRenderer>();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+	}
+
+	void OnMouseOver()
+	{
+		SpriteRenderer.enabled = true;
+	}
+
+	void OnMouseExit()
+	{
+		SpriteRenderer.enabled = false;
 	}
 }
