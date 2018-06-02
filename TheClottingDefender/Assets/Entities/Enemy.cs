e748a3dbb09ca599ac9369f532b9d3e1156393f6
3m5.Stefan Beyer <Stefan.Beyer@3m5.de>
2018-06-02 19:04:40 +0200

    enemy should move now

--- TheClottingDefender/Assets/Entities/Enemy.cs
+++ TheClottingDefender/Assets/Entities/Enemy.cs
@@ -4,29 +4,41 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+	public int MaxHealth = 100;
+	public int RegenerationRate;
+	public float MaxMoveSpeed = 1f;
 
-	public int Health = 100;
-	private int CurrentHealth;
-	public int RegenerationRate = 0;
-	
+	private int currentHealth;
+	private Rigidbody2D body;
+
 	// Use this for initialization
-	void Start ()
+	void Start()
 	{
-		CurrentHealth = Health;
+		currentHealth = MaxHealth;
+		body = GetComponent<Rigidbody2D>();
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		if (CurrentHealth < Health)
-		{
-			CurrentHealth += RegenerationRate;
-		}
+	void Update()
+	{
+		RegenerateHealth();
+		AddMovement();
+	}
+
+	private void RegenerateHealth()
+	{
+		if (currentHealth < MaxHealth) currentHealth += RegenerationRate;
+	}
+
+	private void AddMovement()
+	{
+		if (body.velocity.sqrMagnitude < MaxMoveSpeed * MaxMoveSpeed) body.AddForce(Vector2.right);
 	}
 
 	public void TakeDamage(int dmg)
 	{
-		CurrentHealth -= dmg;
-		if (CurrentHealth <= 0)
+		currentHealth -= dmg;
+		if (currentHealth <= 0)
 		{
 			Destroy(gameObject);
 		}
